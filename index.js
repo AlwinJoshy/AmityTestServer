5d68ae41b6e9828b8adf74156f9d702e258ce7fa
Alwin_Joshy <flamencoresonance@gmail.com>
2019-10-17 15:08:54 +0400

    small changes

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("server test");
+let io = require('socket.io')(process.env.PORT || 80)
+
+console.log('server started')
+
+io.on('connection', function(socket){
+    console.log('connection made....');
+
+    // a circular transmission
+    socket.on('collect', function(data){
+        console.log('getting data...');
+        socket.emit('recive', data)
+    })
+
+    socket.on('disconnect', function(){
+        console.log('connection broken...');
+    })   
+})
